938139c14b599ea2732955274b40a29dd7b69aec
Arif Khan <arif.khan@bakingo.com>
2017-08-23 16:02:23 +0530

    Latest commit

--- tests/Feature/CreateThreadTest.php
+++ tests/Feature/CreateThreadTest.php
@@ -15,9 +15,9 @@ class CreateThreadTest extends TestCase
     public function an_authenticated_user_can_create_a_thread()
     {
         $this->signIn();
-        $thread = create('App\Thread');
-        $this->post('/threads', $thread->toArray());
-        $this->get($thread->path())
+        $thread = make('App\Thread');
+        $response = $this->post('/threads', $thread->toArray());
+        $this->get($response->headers->get('Location'))
                 ->assertSee($thread->title)
                 ->assertSee($thread->body);
     }
@@ -30,5 +30,36 @@ class CreateThreadTest extends TestCase
             ->assertRedirect('/login');
         $this->post('/threads', [])
             ->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function a_thread_requires_title()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_thread_requires_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    public function a_thread_valid_channel_id()
+    {
+        factory('App\Channel', 2)->create();
+        $this->publishThread(['channel_id' => null])
+            ->assertSessionHasErrors('channel_id');
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides)
+    {
+        $this->withExceptionHandling()->signIn();
+        $thread = make('App\Thread', $overrides);
+        return $this->post('threads', $thread->toArray());
     }
 }
