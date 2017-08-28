09679a911fff287263ae0015ba45e4a99e8cab22
Arif Khan <arifkhan.incaendo@gmail.com>
2017-08-28 23:14:39 +0530

    Lesson 23 10:00

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,29 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<div class="page-header">
-			<h1>
-				{{ $profileUser->name }}
-			</h1>
-			<small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-		</div>
-		@foreach ($threads as $thread)
-			<div class="panel panel-default">
-	            <div class="panel-heading">
-	                <div class="level">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>
+                        {{ $profileUser->name }}
+                    </h1>
+                    <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+                </div>
+                @foreach ($threads as $thread)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="level">
 	                	<span class="flex">
 	                		<a href="#">{{ $thread->creator->name }}</a> posted:
-	                		{{ $thread->title }}
+                            {{ $thread->title }}
 	                	</span>
-	                	<span>{{ $thread->created_at->diffForHumans() }}</span>
-	                </div>
-	            </div>
-	            <div class="panel-body">
-	                <div class="body">{{ $thread->body }}</div>
-	            </div>
-	        </div>
-		@endforeach
-		{{ $threads->links() }}
-	</div>
+                                <span>{{ $thread->created_at->diffForHumans() }}</span>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <div class="body">{{ $thread->body }}</div>
+                        </div>
+                    </div>
+                @endforeach
+                {{ $threads->links() }}
+            </div>
+        </div>
+    </div>
 @endsection
